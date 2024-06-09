d1a05fb10441c8c7576cbd9012f9f32f9f1b6618
lourdesberon <147950289+lourdesberon@users.noreply.github.com>
2024-06-09 03:57:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -242,33 +242,3 @@ function displayFavoriteEmojis() {
     });
 }
 
-// service-worker.js
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('static-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/app.js'
-            ]);
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch(error => console.log('Service Worker Registration Failed:', error));
-}
-
-
-
